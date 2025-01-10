826b89763ea65a45330d4c59e43c89983326ab50
kamangir <arash@kamangir.net>
2025-01-09 20:04:21 -0800

    pipeline fix, hopefully - kamangir/bolt#746

--- blue_sandbox/help/microsoft_building_damage_assessment/__init__.py
+++ blue_sandbox/help/microsoft_building_damage_assessment/__init__.py
@@ -1,6 +1,7 @@
 from blue_sandbox.help.microsoft_building_damage_assessment.ingest import (
     help_functions as help_ingest,
 )
+from blue_sandbox.help.microsoft_building_damage_assessment.ingest import list_of_events
 
 help_functions = {
     "ingest": help_ingest,
