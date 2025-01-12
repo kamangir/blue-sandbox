fb4fcddfb70ae62c3063763e6d33561c8c4a4352
kamangir <arash@kamangir.net>
2025-01-11 20:32:47 -0800

    doc++ - kamangir/bolt#746

--- blue_sandbox/list.py
+++ blue_sandbox/list.py
@@ -3,7 +3,7 @@ list_of_experiments = {
         "ICON": "🌐",
         "title": "Satellite imagery damage assessment workflow",
         "url": "https://github.com/kamangir/blue-sandbox/blob/main/blue_sandbox/microsoft_building_damage_assessment/README.md",
-        "marquee": "https://github.com/microsoft/building-damage-assessment/raw/main/figures/damage.png",
+        "marquee": "https://github.com/kamangir/assets/raw/main/blue-sandbox/Maui-Hawaii-fires-Aug-23-ingest-2025-01-10-qqJqhm.png?raw=true",
         "status": "🎰",
     },
     "template": {
