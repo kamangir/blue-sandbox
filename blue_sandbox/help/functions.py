0ae20cefaccb4cba77fc76fa2a13bbc570b7af83
kamangir <arash@kamangir.net>
2025-02-21 19:43:00 -0800

    ++ - kamangir/bolt#746

--- blue_sandbox/help/functions.py
+++ blue_sandbox/help/functions.py
@@ -1,13 +1,6 @@
 from abcli.help.generic import help_functions as generic_help_functions
 
 from blue_sandbox import ALIAS
-from blue_sandbox.help.blue_sam import help_functions as help_blue_sam
 
 
 help_functions = generic_help_functions(plugin_name=ALIAS)
-
-help_functions.update(
-    {
-        "blue_sam": help_blue_sam,
-    }
-)
