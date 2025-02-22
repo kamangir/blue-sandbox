df35be0cc5c3908f30e55e22a4b776ab78e448fc
kamangir <arash@kamangir.net>
2025-02-21 19:40:39 -0800

    ++ - kamangir/bolt#746

--- blue_sandbox/.abcli/alias.sh
+++ blue_sandbox/.abcli/alias.sh
@@ -1,11 +1,3 @@
 #! /usr/bin/env bash
 
-alias @damages=blue_sandbox_microsoft_building_damage_assessment
-
-alias sagesemseg=blue_sandbox_sagesemseg
-
 alias @sandbox=blue_sandbox
-
-alias gpt=openai_commands_gpt
-
-alias @sam=blue_sandbox_blue_sam
