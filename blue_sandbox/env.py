14969ad1cb59993881d0c34835a35e5c987092bd
kamangir <arash@kamangir.net>
2025-03-23 13:41:36 -0700

    basic file upload/download - kamangir/bolt#746

--- blue_sandbox/env.py
+++ blue_sandbox/env.py
@@ -1,30 +1,25 @@
-import os
-from blue_options.env import load_config, load_env
+from blue_options.env import load_config, load_env, get_env
 
 load_env(__name__)
 load_config(__name__)
 
 
-DAMAGES_TEST_DATASET_OBJECT_NAME = os.getenv(
-    "DAMAGES_TEST_DATASET_OBJECT_NAME",
-    "",
+DAMAGES_TEST_DATASET_OBJECT_NAME = get_env("DAMAGES_TEST_DATASET_OBJECT_NAME")
+
+ENCODED_BLOB_SAS_TOKEN = get_env("ENCODED_BLOB_SAS_TOKEN")
+
+SAGESEMSEG_COMPLETED_JOB_pascal_voc_v1_debug_v2 = get_env(
+    "SAGESEMSEG_COMPLETED_JOB_pascal_voc_v1_debug_v2"
 )
 
-ENCODED_BLOB_SAS_TOKEN = os.getenv("ENCODED_BLOB_SAS_TOKEN", "")
-
-SAGESEMSEG_COMPLETED_JOB_pascal_voc_v1_debug_v2 = os.getenv(
-    "SAGESEMSEG_COMPLETED_JOB_pascal_voc_v1_debug_v2",
-    "",
+SAGESEMSEG_COMPLETED_JOB_pascal_voc_v1_full_v2 = get_env(
+    "SAGESEMSEG_COMPLETED_JOB_pascal_voc_v1_full_v2"
 )
 
-SAGESEMSEG_COMPLETED_JOB_pascal_voc_v1_full_v2 = os.getenv(
-    "SAGESEMSEG_COMPLETED_JOB_pascal_voc_v1_full_v2",
-    "",
-)
+GROQ_API_KEY = get_env("GROQ_API_KEY", "")
 
-GROQ_API_KEY = os.getenv("GROQ_API_KEY", "")
+VISUALYZE_PORT = get_env("VISUALYZE_PORT")
 
-VISUALYZE_PORT = os.getenv(
-    "VISUALYZE_PORT",
-    "",
-)
+WEBDAV_HOSTNAME = get_env("WEBDAV_HOSTNAME")
+WEBDAV_LOGIN = get_env("WEBDAV_LOGIN")
+WEBDAV_PASSWORD = get_env("WEBDAV_PASSWORD")
